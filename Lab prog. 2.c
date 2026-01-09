3b21177ebf2bf278fec618bbc5c2f9a87bdf4a97
Abhishek Rai A <abhirai2006@gmail.com>
2026-01-09 17:44:22 +0530

    Implement string pattern matching and replacement
    Added a program to perform string pattern matching and replacement without using built-in functions.

--- Lab prog. 2.c
+++ Lab prog. 2.c
@@ -1,3 +1,15 @@
+/**
+2. Design, Develop and Implement a Program in C for the following
+operations on Strings:
+a. Read a main String (STR), a Pattern String (PAT) and a Replace
+String (REP)
+b. Perform Pattern Matching Operation: Find and Replace all occurrences
+of PAT in STR with REP if PAT exists in STR. Report
+suitable messages in case PAT does not exist in STR. Support the
+program with functions for each of the above operations. Don’t use
+Built-in functions
+**/
+
 #include <stdio.h>
 #include <string.h>
 
@@ -58,3 +70,4 @@ void main()
 
     return 0;
 }
+
