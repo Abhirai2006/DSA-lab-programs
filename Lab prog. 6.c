b0829fb0534d01dfe3ec1e5fff02c1e13ffa4640
Abhishek Rai A <abhirai2006@gmail.com>
2026-01-09 17:48:09 +0530

    Implement circular queue operations for characters
    This program implements a circular queue for characters with operations to enqueue, dequeue, and display elements. It handles overflow and underflow conditions appropriately.

--- Lab prog. 6.c
+++ Lab prog. 6.c
@@ -1,3 +1,15 @@
+/******************************************
+6.  Design, Develop and Implement a menu driven Program in C for the
+    following operations on Circular QUEUE of Characters (Array Implementation
+    of Queue with maximum size MAX):
+    a. Insert an Element on to Circular QUEUE
+    b. Delete an Element from Circular QUEUE
+    c. Demonstrate Overflow and Underflow situations on Circular QUEUE
+    d. Display the status of Circular QUEUE
+    e. Exit. Support the program with appropriate functions for each of
+       the above operations.
+*******************************************/
+
 #include<stdio.h>
 #include<conio.h>
 #define N 5
@@ -92,3 +104,4 @@ void display()
         printf("%c\n",queue[r]);
     }
 }
+
